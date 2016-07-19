b5d3dbc649254942a76e1c437a7a74a85e2715e2
funatsu <keisuekyan2012@gmail.com>
2016-07-19 15:12:39 +0900

    not working...

---
+++ PlaygroundApp/index.ios.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import React, { Component } from 'react';
+import PlaygroundApp from './src/PlaygroundApp';
+import {
+  AppRegistry,
+  View
+} from 'react-native';
+
+AppRegistry.registerComponent('PlaygroundApp', () => PlaygroundApp);
