3393af8e443b60fb2f1cab08c074c95141a03d99
funatsu <keisuekyan2012@gmail.com>
2016-09-19 22:05:51 +0900

    drawer added

--- realmPlayground/app/containers/app.js
+++ realmPlayground/app/containers/app.js
@@ -9,47 +9,71 @@ import {
   Switch,
   RefreshControl,
   ScrollView,
+  TouchableOpacity,
 } from 'react-native'
 
 import RealmTodo from '../components/realmTodo'
-import { ListView } from 'realm/react-native'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 import CustomTabbar from '../components/customTabBar'
 import {Actions, Scene, Router} from 'react-native-router-flux'
 import Drawer from 'react-native-drawer';
+import Button from 'react-native-button';
 
+class TestDrawer extends Component {
+  openDrawer = () => {
+    this._drawer.open()
+  };
+  closeDrawer = () => {
+    this._drawer.close()
+  };
+  render() {
+    return (
+    <Drawer
+        ref={(ref) => this._drawer = ref}
+        content={<TestClass closeDrawer={this.closeDrawer}/>}
+        tapToClose={true}        
+        >
+        <View style={{flex: 1,justifyContent: 'center',}}>        
+        <Text style={{textAlign: 'center',marginTop: 30,}}>{this.props.text}</Text>  
+        <View style={{alignItems: 'center',}}>
+          <TouchableHighlight            
+            style={styles.button}
+            onPress={() => { this.openDrawer() }}
+            activeOpacity={75 / 100}
+            underlayColor={"rgba(47,56,78,1)"}>
+            <Text>Open</Text>
+          </TouchableHighlight>
+        </View>        
+      </View>
+      </Drawer>
+    )
+  }
+}
 class TestClass extends Component {
   render(){
-    const {title, subTitle,} = this.props
-    return(
-      <View style={{
-          flex: 1,
-          justifyContent: 'center',          
-        }}>
-        <Text style={{textAlign: 'center',}}>{this.props.text}</Text>       
+    const {closeDrawer} = this.props
+    return (
+      <View style={styles.container}>
+        <Text style={styles.controlText}>Control Panel</Text>
+        <Button onPress={Actions.todo}>
+          Todo
+        </Button>
+        <Button onPress={closeDrawer}>
+          go back to first page
+        </Button>
       </View>
     )
   }
 } 
-
-class TestDrawer extends Component {
-  render() {
-    const children = this.props.navigationState.children;
-    return(
-      <View>
-      
-      </View>
-    )
-  }
-}
 class TodoWithTab extends Component {
   render() {
-    return (
+    return (      
+      
       <ScrollableTabView style={{marginTop: 40, borderTopWidth: 2, }} 
         tabBarPosition='bottom'
         initialPage={0}
         renderTabBar={() => <CustomTabbar tabList={['Active', 'Completed', 'All']}/>}
-        >
+        >        
         <RealmTodo tabLabel={'navicon'} 
           visibility={'show_active'} 
         />
@@ -70,7 +94,7 @@ export default class App extends Component {
     return (      
      <Router>
       <Scene key="root">        
-        <Scene key="start" component={TestClass} title="Start" text="page1" initial={true} onRight={() => Actions.todo()}  rightTitle="todo"/>
+        <Scene key="start" component={TestDrawer} title="Start" text="page1" initial={true} onRight={() => Actions.todo()}  rightTitle="todo"/>
         <Scene key="todo" component={TodoWithTab} title="Todo" text="page2" hideNavBar={false} />        
         </Scene>
     </Router>
@@ -97,6 +121,22 @@ const styles = StyleSheet.create({
   completed: {
     textDecorationLine: 'line-through',
     textDecorationStyle: 'solid',
+  },
+  button: {
+    width: 100,
+   height: 30,
+   padding: 10,
+   backgroundColor: 'lightgray',
+   alignItems: 'center',
+   justifyContent: 'center',
+   margin: 3
+  },
+  controlPanel: {
+    flex: 1,
+    backgroundColor:'transparent',
+  },
+  controlPanelText: {
+    color:'white',
   },
 });
 
